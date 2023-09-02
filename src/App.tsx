64cb5e8877c4f843a6ea191a1bda9056215cef59
Semen Aleksandrovskii <psifisher@gmail.com>
2023-09-02 11:13:02 +0300

    styles, keyboard controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [timer, setTimer] = useState(() => {
@@ -7,21 +8,58 @@ function App() {
     return newDate;
   });
 
-  const [timeout, setTimeout] = useState<number | null>(null);
+  const [timeout, setTimeout] = useState<number>(15);
   const [interval, setIntervalValue] = useState<NodeJS.Timer | null>(null);
   const [isFinished, setIsFinished] = useState(false);
   const formattedTime = timer.toTimeString().split(" ")[0];
-  function clearTimer() {
-    if (interval) {
-      clearInterval(interval);
-      setIntervalValue(null);
+
+  const handleKeyPress = (event: KeyboardEvent) => {
+    console.log("key pressed", event.key);
+    switch (event.key) {
+      case "r":
+        runTimer();
+        event.stopPropagation();
+        break;
+      case "s":
+        stopTimer();
+        break;
+      case "c":
+        clearTimer();
+        break;
+      default:
+        break;
     }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  });
+
+  const runTimer = () => {
+    if (!interval) {
+      if (!!timeout) {
+        const newDate = new Date();
+        newDate.setHours(0, timeout, 0);
+        setTimer(newDate);
+      }
+      const intervalNumber = setInterval(updateTimer, 1000);
+      setIsFinished(false);
+      setIntervalValue(intervalNumber);
+    }
+  };
+
+  const clearTimer = () => {
+    stopTimer();
     const newDate = new Date();
     newDate.setHours(0, 0, 0);
     setTimer(newDate);
-  }
+  };
 
   function stopTimer() {
+    console.log("interval", interval);
     if (interval) {
       clearInterval(interval);
       setIntervalValue(null);
@@ -35,36 +73,44 @@ function App() {
       return newTimerValue;
     });
   };
+
   if (timer.getSeconds() === 0 && timer.getMinutes() === 0 && !!interval) {
     setIsFinished(true);
     clearTimer();
   }
+
   return (
-    <div>
+    <div className="app-container">
       <h1>{formattedTime}</h1>
-      <h2>{isFinished ? "Finished" : null}</h2>
-      <input
-        type="number"
-        onChange={(e) => setTimeout(parseInt(e.target.value))}
-      />
-      <button
-        onClick={() => {
-          if (!interval) {
-            if (!!timeout) {
-              const newDate = new Date();
-              newDate.setHours(0, timeout, 0);
-              setTimer(newDate);
+      <h2>{isFinished ? "Finished!" : null}</h2>
+      <label>
+        Start pomidor for&nbsp;
+        <input
+          type="number"
+          className="timeout-input"
+          autoFocus
+          value={timeout}
+          data-e2e="timeout-input"
+          onChange={(e) => {
+            if (e.target.value && parseInt(e.target.value) > 0) {
+              setTimeout(parseInt(e.target.value));
             }
-            const intervalNumber = setInterval(updateTimer, 1000);
-            setIsFinished(false);
-            setIntervalValue(intervalNumber);
-          }
-        }}
-      >
-        Start
-      </button>
-      <button onClick={stopTimer}>Stop</button>
-      <button onClick={clearTimer}>Clear</button>
+          }}
+        />
+        &nbsp;minutes
+      </label>
+
+      <div className="control-panel">
+        <button accessKey="r" className="control-button" onClick={runTimer}>
+          Run
+        </button>
+        <button accessKey="s" className="control-button" onClick={stopTimer}>
+          Stop
+        </button>
+        <button accessKey="c" className="control-button" onClick={clearTimer}>
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
