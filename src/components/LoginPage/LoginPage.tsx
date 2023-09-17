9c1fd860e3df196ac87db94e35f6c6f4d360cecc
Semen Aleksandrovskii <psifisher@gmail.com>
2023-09-17 15:54:11 +0300

    login requests

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,11 +1,33 @@
+import { login } from "../../api/api";
 import AppContainer from "../common/AppContainer/AppContainer";
-import { LoginForm } from "./LoginForm";
+import { LoginForm, LoginInputs } from "./LoginForm";
+import React from "react";
 
 export function LoginPage() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | null>(null);
+
+  async function onSubmit(data: LoginInputs) {
+    login(data)
+      .then((res) => {
+        setIsLoggedIn(true);
+        console.log("Logged in!", res);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoggedIn(false);
+      });
+  }
+
   return (
     <AppContainer>
       <h2>Login</h2>
-      <LoginForm />
+      {error && <p>{error}</p>}
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <LoginForm onSubmit={onSubmit} />
+      )}
     </AppContainer>
   );
 }
